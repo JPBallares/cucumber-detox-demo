79e84ff9ff67a6120749e990c9b8316e70033365
Justine Ballares <ballaresjustine@gmail.com>
2022-12-14 00:13:50 +0800

    updated the package versions

--- cucumber/Find.step.js
+++ cucumber/Find.step.js
@@ -1,5 +1,6 @@
-import { Then, Given } from '@cucumber/cucumber';
+import { Then, Given, setDefaultTimeout } from '@cucumber/cucumber';
 import { expect, element, by, waitFor } from 'detox';
+setDefaultTimeout(60 * 1000);
 
 Given('I should see the {string} element', async (elementId) => {
   await expect(element(by.id(elementId))).toBeVisible();
